c1981296dbeb26e5c3246218433927697639c167
git-pyotr <g.pyotr@gmail.com>
2017-11-22 02:50:04 +0500

    Скрол

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (function() {
 
+var body = document.querySelector("body");
 var preview = document.querySelectorAll(".preview");
 var item = document.querySelector(".module");
 var itemImage = document.querySelector(".item-image");
@@ -8,16 +9,18 @@ var ancor = document.querySelectorAll(".menu-item");
 
 item.addEventListener("click", function(event) {
   event.preventDefault();
+  body.classList.toggle("scroll-off");
   item.classList.toggle("show");	
 });
 
 for (var i = 0; i < preview.length; i++) {
 	preview[i].addEventListener("click", function(event) {
     event.preventDefault();
-    var elemPosition = window.pageYOffset + 50;
+    var elemPosition = window.pageYOffset;
     item.classList.toggle("show");	
     image.src = "img/" + this.alt;
-    itemImage.style = "margin-top: " + elemPosition + "px";
+    item.style = "top: " + elemPosition + "px";
+    body.classList.toggle("scroll-off");
 	});
 }
 
