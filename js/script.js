c844365e7eb4a119c816a647fd8723a5e38cae71
git-pyotr <g.pyotr@gmail.com>
2018-01-12 17:31:37 +0500

    Редизайн

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 (function() {
 
 var body = document.querySelector("body");
-var preview = document.querySelectorAll(".preview");
+var preview = document.querySelectorAll(".item-preview");
 var item = document.querySelector(".module");
 var itemImage = document.querySelector(".item-image");
 var image = document.querySelector(".image");
 var ancor = document.querySelectorAll(".menu-item");
+
+function showModule(elem) {
+  var elemPosition = window.pageYOffset;
+  item.classList.toggle("show");	
+  image.src = "img/" + elem.alt;
+  item.style = "top: " + elemPosition + "px";
+  body.classList.toggle("scroll-off");
+}
 
 item.addEventListener("click", function(event) {
   event.preventDefault();
@@ -16,21 +24,18 @@ item.addEventListener("click", function(event) {
 for (var i = 0; i < preview.length; i++) {
 	preview[i].addEventListener("click", function(event) {
     event.preventDefault();
-    var elemPosition = window.pageYOffset;
-    item.classList.toggle("show");	
-    image.src = "img/" + this.alt;
-    item.style = "top: " + elemPosition + "px";
-    body.classList.toggle("scroll-off");
+
+    showModule(this.firstElementChild);
 	});
 }
 
-/* for (var i = 0; i < ancor.length; i++) {
+for (var i = 0; i < ancor.length; i++) {
 	ancor[i].addEventListener("click", function(event) {
     if (document.querySelector(".active")) {
       document.querySelector(".active").classList.remove("active");
     }
     this.classList.add("active");
 	});
-} */
+} 
 
 })();
